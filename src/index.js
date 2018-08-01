966b457d5bd00af76a038855a949920730f93ab9
shtokhamer <shtokhamer@gmail.com>
2018-08-01 08:46:50 +0300

    Starting out with react bootstrap.

--- src/index.js
+++ src/index.js
@@ -1,31 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import FormGroup from "react-bootstrap";
-import FormControl from "react-bootstrap";
-import ControlLabel from "react-bootstrap";
-import HelpBlock from "react-bootstrap";
+import { FormGroup } from "react-bootstrap";
+import { ControlLabel } from "react-bootstrap";
+import { FormControl } from "react-bootstrap";
+import { HelpBlock } from "react-bootstrap";
+import { Form } from "react-bootstrap";
 
 import "./styles.css";
 
 function App() {
   return (
     <div className="App">
-      <form>
-        <FormGroup
-          controlId="formBasicText"
-          validationState={this.getValidationState()}
-        >
-          <ControlLabel>Working example with validation</ControlLabel>
-          <FormControl
-            type="text"
-            value={this.state.value}
-            placeholder="Enter text"
-            onChange={this.handleChange}
-          />
+      <Form>
+        <FormGroup controlId="formBasicText">
+          <ControlLabel>Day 1 Title</ControlLabel>
+          <FormControl type="text" value="" placeholder="Enter description" />
           <FormControl.Feedback />
           <HelpBlock>Validation is based on string length.</HelpBlock>
         </FormGroup>
-      </form>
+      </Form>
     </div>
   );
 }
