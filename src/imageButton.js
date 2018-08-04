c0e3b7f37e056c471e4c97679a0b628456b3def2
shtokhamer <shtokhamer@gmail.com>
2018-08-04 11:58:48 +0300

    Reached a good point of panel design.

--- src/imageButton.js
+++ src/imageButton.js
@@ -4,7 +4,9 @@ import "./imageButton.css";
 const ImageButton = ({ imageSrc, textBelow, imageId }) => (
   <div className="image-button">
     <img src={imageSrc} className="panel-image" alt="" />
-    <label for={imageId}>{textBelow}</label>
+    <label for={imageId} className="panel-image-label">
+      {textBelow}
+    </label>
   </div>
 );
 
